c783e9c01955d62bb341bc71389284f3df461069
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-05-24 09:12:23 -0300

    ok

--- trigger.c
+++ trigger.c
@@ -2,6 +2,7 @@
 #include "stdlib.h"
 #include "string.h"
 
+
 struct Imagem {
     int linhas;
     int colunas;
@@ -9,10 +10,24 @@ struct Imagem {
 };
 
 struct RGB {
-    short int vermelho;
-    short int verde;
-    short int azul;
+    unsigned char vermelho;
+    unsigned char verde;
+    unsigned char azul;
 };
+
+struct RGB compacta(int len_x, int len_y, struct RGB *matriz[len_x][len_y]){
+
+    int split_x = len_x / 2;
+    int split_y = len_y / 2;
+
+    if(len_x <= 3 || len_y <= 3)
+        return *matriz[(int)(split_x)][(int)(split_y)];
+
+    compacta(split_x, split_y, matriz[0][0]);
+    compacta(split_x, split_y, &matriz[0][split_y]);
+    compacta(split_x, split_y, &matriz[split_x][0]);
+    compacta(split_x, split_y, &matriz[split_x][split_y]);
+}
 
 
 int main(int argc, char *argv[]){
@@ -46,9 +61,9 @@ int main(int argc, char *argv[]){
 
 
     struct Imagem imagem = {
-        .linhas = (int)image[18] + 1,
-        .colunas = (int)image[22] + 1,
-        .offset = (int)image[10] + 1
+        .linhas = (int)image[18],
+        .colunas = (int)image[22],
+        .offset = (int)image[10]
     };
 
     printf("\nNúmero de linhas: %d (0x%02X)\nNumero de colunas: %d (0x%02X)\n\nComeço da Imagem: %d (0x%02X)\n", imagem.linhas, imagem.linhas, imagem.colunas, imagem.colunas, imagem.offset, imagem.offset);
@@ -57,64 +72,27 @@ int main(int argc, char *argv[]){
 
     struct RGB matriz[imagem.linhas][imagem.colunas];
 
+    int padding = (4 - (imagem.linhas % 4)) % 4;
+    for(int i = 0, j = 0; i < imagem.colunas; i++){
+        for(int k = 0; k < (imagem.linhas); j += 3, k++){
+
+            matriz[k][i].verde = image[j];
+            matriz[k][i].azul = image[j + 1];
+            matriz[k][i].vermelho = image[j + 2];
+
+            printf("%d - Green: %02x (%d) ", k, image[j], i);
+            printf(" Blue: %02x (%d) ", image[j + 1], i);
+            printf(" Red: %02x (%d) %02X\n", image[j + 2], i, j);
+        }
+        j += padding;
+    }
+
     
-
-    for(int i = 0, j = 0, l = 0; i < ((imagem.colunas) * (imagem.linhas)) * 3; i++){
-        if(i % 3 == 0){
-            matriz[l][j].vermelho = image[i];
-           printf("%d - Red: %02x (%d) ", l, image[i], j);
-        }
-        else if(i % 3 == 1){
-            matriz[l][j].verde = image[i];
-            printf("Green: %02x (%d) ", image[i], j);
-        }
-        else{
-            matriz[l][j].azul = image[i];
-            printf("Blue: %02x (%d) \n", image[i], j);
-            j++;
-            if(j == imagem.colunas){
-                j = 0;
-                l++;
-            }
-        }   
-    }
-
-    for(int i = 0; i < imagem.colunas; i++){
+    for(int i = 0; i < imagem.linhas; i++){
         for(int j = 0; j < imagem.colunas; j++){
-            //printf("#%02X%02X%02X\n", matriz[i][j].vermelho, matriz[i][j].verde, matriz[i][j].azul); 
+           printf("\"#%02X%02X%02X\",\n", matriz[i][j].vermelho, matriz[i][j].verde, matriz[i][j].azul); 
         }
     }
-
-
-
-
 
     return 0;
 }
-
-/*
-
-struct RGB compacta_lossy(int len_x, int len_y, unsigned char matriz[len_y][len_x], int soma_x, int soma_y){
-    printf("\nEnd\n\n");
-
-    int setor_x = len_x / 2;
-    int setor_y = len_y / 2;
-    if( setor_x >= 3 && setor_y / 2 >= 3){
-      // compacta_lossy(setor_x, setor_y, matriz);
-    }    //compacta_lossy(imagem.linhas, imagem.colunas, imagem_matriz);
-
-        int i, j = 0;
-        for(i = 0; i < 0x36; i++)
-            printf("%02X ", image[i]);
-        printf("\n");
-        for(i=0; i < 20; i++)
-        {
-          for (j=0; j<22; j++)
-            printf("%02X%02x%02x ", image[0x36+i*22*3+j*3], image[0x36+i*22*3+j*3+1], image[0x36+i*22*3+j*3+2]);
-        printf ("\n");
-        }
-
-
-            unsigned char (*imagem_matriz)[imagem.linhas] = (unsigned char (*)[imagem.linhas])&image[imagem.offset];
-
-}*/
