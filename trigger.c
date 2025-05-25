14dd9ad3e11a45c0bb0a66dbb93253b18f9cb3d9
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-05-25 20:26:05 -0300

    o que tá pronto

--- trigger.c
+++ trigger.c
@@ -20,7 +20,7 @@ struct RGB compacta(int len_x, int len_y, struct RGB matriz[len_x][len_y], unsig
     int split_x = len_x / 2;
     int split_y = len_y / 2;
 
-    printf("#%02X%02X%02X - <%d,%d>\n", matriz[split_x][split_y].vermelho, matriz[split_x][split_y].verde, matriz[split_x][split_y].azul, split_x,split_y);
+    //printf("#%02X%02X%02X - <%d,%d>\n", matriz[split_x][split_y].vermelho, matriz[split_x][split_y].verde, matriz[split_x][split_y].azul, split_x,split_y);
 
     *(buffer + 0) = matriz[split_x][split_y].verde;
     *(buffer + 1) = matriz[split_x][split_y].azul;
@@ -32,28 +32,28 @@ struct RGB compacta(int len_x, int len_y, struct RGB matriz[len_x][len_y], unsig
         //printf("where %02x\n", matriz[(int)(split_x)][(int)(split_y)].verde);
         //return matriz[(int)(split_x)][(int)(split_y)];
         // printf("Base: %p\n", buffer);
-        return matriz[(int)(split_x)][(int)(split_y)];
+        printf("#%02X%02X%02X - <%d,%d>\n", matriz[split_x][split_y].vermelho, matriz[split_x][split_y].verde, matriz[split_x][split_y].azul, split_x,split_y);
+        return matriz[(int)(split_x + 1)][(int)(split_y + 1)];
     }
 
     //printf("Setor 1: \n");
-    compacta(split_x, split_y, (struct RGB (*)[])matriz , (buffer) + 3);
+    compacta(split_x, split_y, (struct RGB (*)[])&matriz , (buffer) + 3);
 
     
     // printf("Setor 3: \n");
-    //compacta(split_x, split_y, (struct RGB (*)[split_x])&matriz[split_x], buffer - 3);
+    compacta(split_x, split_y, (struct RGB (*)[split_x])&matriz[split_x], buffer - 3);
     /*
         *(buffer_i + 3) = matriz[split_x][0].verde;
     *(buffer_i + 4) = matriz[split_x][0].azul;
     *(buffer_i + 5) = matriz[split_x][0].vermelho;*/
     //printf("Setor 2: \n");
-    //buffer += 9;
-    //compacta(split_x, split_y, (struct RGB (*)[split_y])&matriz[split_y], buffer - 3);
+    compacta(split_x, split_y, (struct RGB (*)[split_y])&matriz[split_y], buffer - 3);
     /*
         *(buffer_i + 6) = matriz[split_x][0].verde;
     *(buffer_i + 7) = matriz[split_x][0].azul;
     *(buffer_i + 8) = matriz[split_x][0].vermelho;*/
     //printf("Setor 4: \n");
-    // compacta(split_x, split_y, (struct RGB (*)[])&matriz[split_x][split_y], buffer);
+    compacta(split_x, split_y, (struct RGB (*)[])&matriz[split_x][split_y], buffer);
     /*
         *(buffer_i + 9) = matriz[split_x][split_y].verde;
     *(buffer_i + 10) = matriz[split_x][split_y].azul;
@@ -94,8 +94,8 @@ int main(int argc, char *argv[]){
 
 
     struct Imagem imagem = {
-        .linhas = (int)image[18],
-        .colunas = (int)image[22],
+        .linhas = (int)image[22],
+        .colunas = (int)image[18],
         .offset = (int)image[10]
     };
 
@@ -113,8 +113,8 @@ int main(int argc, char *argv[]){
     for(int i = 0, j = 0; i < imagem.colunas; i++){
         for(int k = 0; k < (imagem.linhas); j += 3, k++){
 
-            matriz[k][i].verde = image[j];
-            matriz[k][i].azul = image[j + 1];
+            matriz[k][i].azul = image[j];
+            matriz[k][i].verde = image[j + 1];
             matriz[k][i].vermelho = image[j + 2];
 
            // printf("%d - Green: %02x (%d) ", k, image[j], i);
@@ -127,14 +127,15 @@ int main(int argc, char *argv[]){
     
     for(int i = 0; i < imagem.linhas; i++){
         for(int j = 0; j < imagem.colunas; j++){
-          // printf("\"#%02X%02X%02X\",\n", matriz[i][j].vermelho, matriz[i][j].verde, matriz[i][j].azul); 
+          printf("#%02X%02X%02X ", matriz[i][j].vermelho, matriz[i][j].verde, matriz[i][j].azul); 
         }
+        printf("\n");
     }
 
-    compacta(imagem.linhas, imagem.colunas, matriz, &compacted_arr[imagem.offset]);
+    //compacta(imagem.linhas, imagem.colunas, matriz, &compacted_arr[imagem.offset]);
 
     for(int i = imagem.offset; i < 100; i++){
-        printf("%02X - %p \n", compacted_arr[i], &compacted_arr[i]); 
+        //printf("%02X - %p \n", compacted_arr[i], &compacted_arr[i]); 
     }
     
 
